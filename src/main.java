169b41c2d6f75177262073d077719aef8423f361
gulci <gulci@LAPTOP-KPNR185M>
2022-09-21 19:33:50 +0300

    commit

---
+++ src/main.java
@@ -0,0 +1,28 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// switch bloklarıyla dallandırma yaparız
+		char grade = 'B';
+
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+
+		case 'C':
+			System.out.println("İyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena Değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Maalesef Kaldınız");
+			break;
+		default:
+			System.out.println("Geçersiz not girdiniz");
+		}
+	}
+
+}
